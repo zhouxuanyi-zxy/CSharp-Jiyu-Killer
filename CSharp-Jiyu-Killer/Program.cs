426ae0823f98df352316c921d6fa41bc41913a3e
zhouxuanyi_zxy <69704410+zhouxuanyi-zxy@users.noreply.github.com>
2024-03-02 19:02:26 +0800

    v1.01

---
+++ CSharp-Jiyu-Killer/Program.cs
@@ -0,0 +1,62 @@
+﻿// Jiyu-Killer C Sharp Edition
+// Version 1.01
+// License: MIT License
+
+using System.Text;
+using CliWrap;
+
+if (is_file_exist()){
+    await kill("taskkill");
+    await kill("pskill");
+    await kill("ntsd");
+}
+
+static bool is_file_exist(){
+    string check_file = "pskill.exe";
+    if (File.Exists(check_file)){
+        check_file = "ntsd.exe";
+        if (File.Exists(check_file)){
+            Console.WriteLine("文件检测完毕!");
+            return true;
+        }
+        else{
+            Console.WriteLine("缺少ntsd.exe!");
+            return false;
+        }
+    }
+    else{
+        Console.WriteLine("缺少pskill.exe");
+        return false;
+    }
+}
+
+async static Task kill(string use_program){
+    var program_output = new StringBuilder();
+    try{
+        if (use_program == "pskill"){
+            var pksill = await Cli.Wrap("pskill.exe")
+                .WithArguments(["-nobanner","-t","StudentMain.exe"])
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(program_output))
+                .ExecuteAsync();
+                
+            // Console.WriteLine(program_output.ToString());
+        }
+        else if (use_program == "ntsd"){
+            var pksill = await Cli.Wrap("ntsd.exe")
+                .WithArguments(["-c","q","-pn","StudentMain.exe"])
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(program_output))
+                .ExecuteAsync();
+        }
+        else if (use_program == "taskkill"){
+            var pksill = await Cli.Wrap("taskkill.exe")
+                .WithArguments(["/F","/T","/IM","StudentMain.exe"])
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(program_output))
+                .ExecuteAsync();
+        }
+    }
+    catch (CliWrap.Exceptions.CommandExecutionException er){
+        Console.WriteLine("Task Not found.");
+        Console.WriteLine(er);
+    }
+    // Console.WriteLine(pskill_o.ToString());
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # CSharp-Jiyu-Killer
+
+[Python-Nanruan-Killer](https://github.com/zhouxuanyi-zxy/Python-Nanruan-Killer) 的C#版
+
+## 为什么用C#重写
+
+因为之后的功能(解除键盘的hook,最小化窗口)还是用C#更加好写一些
+
+## 功能实现情况
+
++ 文件检测
+ 
++ pskill 和 ntsd 杀进程
+
+## 更新历史
+
+#### v1.01 (2024/3/2)
+
+1、首个版本
+
+2、文件检测
+ 
+3、pskill 和 ntsd 杀进程
